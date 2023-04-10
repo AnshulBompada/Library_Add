5ab6c0542d17263fa5e0092e8ca6aa1ad573340b
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2023-04-10 18:52:18 -0500

    Added profile

---
+++ src/main/java/frc/robot/Libs/ProfilePID.java
@@ -0,0 +1,40 @@
+package frc.robot.Libs;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Timer;
+
+public class ProfilePID {
+    private PIDController mPID;
+    private SCurveProfile mprofile;
+    private Timer mTimer;
+
+
+    public ProfilePID(PIDController PID, SCurveProfile profile) {
+        mPID = PID;
+        mprofile = profile;
+        mTimer = new Timer();
+    }
+
+    public PIDController getPID() {
+        return mPID;
+    }
+
+    public SCurveProfile getProfile() {
+        return mprofile;
+    }
+
+    public void reset(double position, double goal) {
+        mPID.reset();
+        mprofile.reset(position);
+        mprofile.setGoal(goal);
+        mTimer.reset();
+        mTimer.start();
+    }
+
+    public double calculate(double position) {
+        double time = mTimer.get();
+        double profile = mprofile.calculate(time);
+        double output = mPID.calculate(position, profile);
+        return output;
+    }
+}
