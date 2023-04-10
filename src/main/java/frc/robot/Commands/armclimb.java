684a4418963792e4d2c5e43e3e3384bc248967c9
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2023-04-10 00:11:29 -0500

    idk

--- src/main/java/frc/robot/Commands/armclimb.java
+++ src/main/java/frc/robot/Commands/armclimb.java
@@ -1,7 +1,9 @@
 package frc.robot.Commands;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class armclimb extends CommandBase {
+  TrapezoidProfile.State m_setpoint;
   public armclimb() {
   }
 
